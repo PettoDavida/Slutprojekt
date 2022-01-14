ff630c43c68b46a34fcaa4bd88c5b4a1166d96f0
matsegard <matsegard@outlook.com>
2022-01-14 13:38:58 +0100

    First commit. Created classes

---
+++ src/game.ts
@@ -0,0 +1,17 @@
+class game {
+ public obstacle: obstacle[]
+ public spaceship: Spaceship
+ public menu: Menu
+ public background: Background
+ public gameState: "start" | "running" | "over"
+
+public draw(){}
+public update(){}
+private spawnObstacle(){}
+private changeBackgroundImage(){}
+private highScore(){}
+private showDistanceOnScreen(){}
+private checkCollision(){}
+}
+
+
---
+++ src/gameovermenu.ts
@@ -0,0 +1,12 @@
+class gameovermenu{
+    private background: string
+    private text: string
+    private size: p5.Vector
+    public gameState: "start" | "running" | "over"
+
+    public draw(){}
+    public update(){}
+    private newGame(){}
+    private checkHighScore(){}
+}
+
