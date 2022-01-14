d19c4add6d8dd355b455ed4d719159f8775fb258
matsegard <matsegard@outlook.com>
2022-01-14 13:40:29 +0100

    First commit. Created classes

---
+++ src/startmenu.ts
@@ -0,0 +1,11 @@
+class menu{
+    private background: string
+    private text: string
+    private size: p5.Vector
+
+    public draw(){}
+    public update(){}
+    private checkControls(){}
+    private checkHighScore(){}
+    private newGame(){}
+}
