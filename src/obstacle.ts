7ba15b6986ddc023f922a0c39aa680328822288f
matsegard <matsegard@outlook.com>
2022-01-19 15:09:41 +0100

    Fixed spawn obstacles delay and speed on spaceship

--- src/obstacle.ts
+++ src/obstacle.ts
@@ -12,14 +12,25 @@ class Obstacle {
 
     public update(horizontalGameSpeed: number) {
         // fart i px per sekund
-        this.position.x -= horizontalGameSpeed * (deltaTime / 700)
+        this.position.x -= horizontalGameSpeed * (deltaTime / 1000)
     }
 
     public draw(){
        image(this.image, this.position.x, this.position.y, this.size.x, this.size.y);
     }
 
+
     private obstacleSize(){}
     private obstaclePosition(){}
     private obstacleType(){}
+    
 }
+
+// när nytt hinder ska spawnas ska den utgå från sista hindret i arrayen för att få fram 
+//dens aktuella x-position och y-position och alltid spawna minst ett spaceshipe-size 
+//från det föregående hindret
+
+// checkLastObjPosition() 
+// sparar x-position i en variabel
+// 
+
