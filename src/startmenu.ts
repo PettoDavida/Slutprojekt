52531cd7b019d15b4e3e5888defb1704517f6a7d
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-01-20 10:13:09 +0100

    position changed on the menu and added a background

--- src/startmenu.ts
+++ src/startmenu.ts
@@ -12,8 +12,9 @@ class Menu {
 
     private static createMenuContainer(): p5.Element {
         return createDiv()
-            .position(0, 0)
-            .size(width, height)
+            .position(350, 50)
+            .size(500, 500)
+            .addClass('startContainer')
             .parent(document.querySelector('main') as Element)
     }
 
@@ -23,23 +24,23 @@ class Menu {
         this.menuContainer = Menu.createMenuContainer()
 
         createElement('h1', 'Space Jam 3.0')
-            .position(600, 300)
+            .position(40, 5)
             .parent(this.menuContainer)
 
         createButton('Start Game')
-            .position(700, 420)
+            .position(150, 140)
             .size(200, 60)
             .mousePressed(this.startGame.bind(this))
             .parent(this.menuContainer)
 
         createButton('Controls')
-            .position(500, 520)
+            .position(0, 240)
             .size(200, 60)
             .mousePressed(this.checkControls.bind(this))
             .parent(this.menuContainer)
 
         createButton('HighScore')
-            .position(900, 520)
+            .position(300, 240)
             .size(200, 60)
             .mousePressed(this.checkHighScore.bind(this))
             .parent(this.menuContainer)
@@ -63,53 +64,53 @@ class Menu {
         this.menuContainer = Menu.createMenuContainer()
 
         createElement('h2', 'Game Controls')
-            .position(600, 50)
+            .position(40, 5)
             .parent(this.menuContainer)
 
         createElement('p', 'Fly')
-            .position(630, 150)
+            .position(80, 100)
             .parent(this.menuContainer)
 
         createElement('p', 'Left Mouse Button')
-            .position(550, 200)
+            .position(0, 150)
             .addClass('gameKeys')
             .parent(this.menuContainer)
 
         createElement('p', 'Space')
-            .position(600, 270)
+            .position(40, 220)
             .addClass('gameKeys')
             .parent(this.menuContainer)
 
         createElement('p', 'W')
-            .position(620, 330)
+            .position(60, 290)
             .addClass('gameKeys')
             .parent(this.menuContainer)
 
         createElement('p', '\u02C4')
-            .position(630, 390)
+            .position(60, 360)
             .addClass('gameKeys')
             .parent(this.menuContainer)
 
         createElement('p', 'Pause')
-            .position(870, 170)
+            .position(430, 170)
             .parent(this.menuContainer)
 
         createElement('p', 'P')
-            .position(800, 150)
+            .position(350, 150)
             .addClass('gameKeys')
             .parent(this.menuContainer)
 
         createElement('p', 'Mute')
-            .position(870, 240)
+            .position(430, 240)
             .parent(this.menuContainer)
 
         createElement('p', 'M')
-            .position(800, 220)
+            .position(350, 220)
             .addClass('gameKeys')
             .parent(this.menuContainer)
 
         createButton('Back')
-            .position(700, 500)
+            .position(200, 450)
             .size(80, 30)
             .mousePressed(this.setup.bind(this))
             .parent(this.menuContainer)
@@ -121,15 +122,31 @@ class Menu {
         this.menuContainer = Menu.createMenuContainer()
 
         const ol = createElement('ol', 'Highscore')
-            .position(550, 150)
+            .position(40, 5)
             .parent(this.menuContainer)
 
         createElement('li', 'Anna')
-            .position(0, 70)
+            .position(100, 70)
+            .parent(ol)
+
+        createElement('li', 'Ella')
+            .position(100, 110)
+            .parent(ol)
+
+        createElement('li', 'Maximilian')
+            .position(100, 150)
+            .parent(ol)
+
+        createElement('li', 'Linnea')
+            .position(100, 190)
+            .parent(ol)
+
+        createElement('li', 'Erik')
+            .position(100, 240)
             .parent(ol)
 
         createButton('Back')
-            .position(700, 500)
+            .position(200, 450)
             .size(80, 30)
             .mousePressed(this.setup.bind(this))
             .parent(this.menuContainer)
